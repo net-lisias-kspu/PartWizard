16dba6c2056613e8f361b8f4fb63529c43692a6c
Jonathan Bayer <linuxgurugamer@gmail.com>
2017-05-28 14:17:14 -0400

    Updated for 1.3

--- buildRelease.bat
+++ buildRelease.bat
@@ -1,31 +1,11 @@
-
-cd
+rem
 
 copy PartWizard\bin\Release\PartWizard.dll GameData\PartWizard\Plugins
 copy PartWizard\PartWizard.version GameData\PartWizard
 copy README GameData\PartWizard
 
- set DEFHOMEDRIVE=d:
-set DEFHOMEDIR=%DEFHOMEDRIVE%%HOMEPATH%
-set HOMEDIR=
-set HOMEDRIVE=%CD:~0,2%
-
 set RELEASEDIR=d:\Users\jbb\release
 set ZIP="c:\Program Files\7-zip\7z.exe"
-echo Default homedir: %DEFHOMEDIR%
-
-rem set /p HOMEDIR= "Enter Home directory, or <CR> for default: "
-
-if "%HOMEDIR%" == "" (
-set HOMEDIR=%DEFHOMEDIR%
-) 
-echo %HOMEDIR%
-
-SET _test=%HOMEDIR:~1,1%
-if "%_test%" == ":" (
-set HOMEDRIVE=%HOMEDIR:~0,2%
-)
-
 
 
 
--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,6 @@
-
-cd
+rem
 
 copy PartWizard\bin\Debug\PartWizard.dll GameData\PartWizard\Plugins
 cd GameData
-xcopy /s/y PartWizard R:\KSP_1.2.2_dev\GameData\PartWizard
+xcopy /s/y PartWizard R:\KSP_1.3.0_dev\GameData\PartWizard
  
